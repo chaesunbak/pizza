c36f70eb6b7271ccfa85e87ebd9d3237eda33bb6
chaesunbak <coanstjd0422@gmail.com>
2026-03-06 23:24:57 +0900

    feat : cn 유틸함수 추가

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export const getHash = (char: string, index: number) => {
   let hash = 0;
   const str = char + index.toString();
