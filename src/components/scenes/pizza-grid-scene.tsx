73a71e22dad52ba6120647d34787dcf86c7fc413
chaesunbak <coanstjd0422@gmail.com>
2026-04-05 12:53:05 +0900

    refactor : row 컴포넌트 분리

--- src/components/scenes/pizza-grid-scene.tsx
+++ src/components/scenes/pizza-grid-scene.tsx
@@ -4,44 +4,48 @@ import { Pizza } from "../pizza";
 
 export function PizzaGridScene() {
   const rows = 4;
-  const pizzasPerRow = 10; 
-
-  const renderRow = (moveRight: boolean) => {
-    const marqueeClass = moveRight ? 'animate-marquee-right' : 'animate-marquee-left';
-    const spinClass = moveRight ? 'animate-spin-slow' : 'animate-spin-slow-reverse';
-
-    return (
-      <div className="flex w-full overflow-hidden">
-        <div className={`flex w-max ${marqueeClass}`}>
-          {/* First set of pizzas */}
-          <div className="flex shrink-0">
-            {Array.from({ length: pizzasPerRow }).map((_, i) => (
-              <div key={`set1-${i}`} className="p-4">
-                <Pizza size={200} className={spinClass} />
-              </div>
-            ))}
-          </div>
-          {/* Duplicated set for seamless marquee */}
-          <div className="flex shrink-0">
-            {Array.from({ length: pizzasPerRow }).map((_, i) => (
-              <div key={`set2-${i}`} className="p-4">
-                <Pizza size={200} className={spinClass} />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    );
-  };
 
   return (
     <div className="fixed inset-0 flex flex-col justify-between py-4 overflow-hidden z-0 pointer-events-none">
       {Array.from({ length: rows }).map((_, i) => (
         <div key={i} className="flex-1 flex items-center">
           {/* i % 2 === 0 is the 1st, 3rd, 5th row (starting from top) */}
-          {renderRow(i % 2 === 0)}
+          <PizzaRow moveRight={i % 2 === 0} />
         </div>
       ))}
+    </div>
+  );
+}
+
+function PizzaRow({ moveRight }: { moveRight: boolean }) {
+  const marqueeClass = moveRight
+    ? "animate-marquee-right"
+    : "animate-marquee-left";
+  const spinClass = moveRight
+    ? "animate-spin-slow"
+    : "animate-spin-slow-reverse";
+  const pizzasPerRow = 10;
+
+  return (
+    <div className="flex w-full overflow-hidden">
+      <div className={`flex w-max ${marqueeClass}`}>
+        {/* First set of pizzas */}
+        <div className="flex shrink-0">
+          {Array.from({ length: pizzasPerRow }).map((_, i) => (
+            <div key={`set1-${i}`} className="p-4">
+              <Pizza size={200} className={spinClass} />
+            </div>
+          ))}
+        </div>
+        {/* Duplicated set for seamless marquee */}
+        <div className="flex shrink-0">
+          {Array.from({ length: pizzasPerRow }).map((_, i) => (
+            <div key={`set2-${i}`} className="p-4">
+              <Pizza size={200} className={spinClass} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
